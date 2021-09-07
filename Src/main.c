5034e2dfc59b0c671b972f88e8330348ec16cf40
blitz2190 <blitz2190@DESKTOP-216274I>
2021-09-07 13:25:59 -0400

    testing - probably broke something?

--- Src/main.c
+++ Src/main.c
@@ -1610,7 +1610,7 @@ if(input > 48 && armed){
 	 			 maskPhaseInterrupts();
 	 			 allpwm();
 	 		 advanceincrement();
-             step_delay = map (input, 48, 137, 7000/motor_poles, 810/motor_poles);
+             step_delay = map (input, 48, sine_mode_changeover, 7000/motor_poles, 810/motor_poles);
 	 		 delayMicros(step_delay);
 
 	 		  }else{
@@ -1621,16 +1621,16 @@ if(input > 48 && armed){
 	 			  stepper_sine = 0;
 	 			  running = 1;
 				  old_routine = 1;
-		 		  commutation_interval = 9000;
-		 		  average_interval = 9000;
-				  last_average_interval = average_interval;
-		 		//  minimum_duty_cycle = ;
-		 		  INTERVAL_TIMER->CNT = 9000;
+		 		  //commutation_interval = 9000;
+		 		  //average_interval = 9000;
+				  //last_average_interval = average_interval;
+		 		  //minimum_duty_cycle = ;
+		 		  //INTERVAL_TIMER->CNT = 9000;
 				  zero_crosses = 0;
 				  prop_brake_active = 0;
 	 			  //step = changeover_step;                    // rising bemf on a same as position 0.
-		 		 //comStep(step);// rising bemf on a same as position 0.
-	 			LL_TIM_GenerateEvent_UPDATE(TIM1);
+		 		  //comStep(step);// rising bemf on a same as position 0.
+	 			  LL_TIM_GenerateEvent_UPDATE(TIM1);
 	 			  zcfoundroutine();
 	 			  }
 	 		  }
