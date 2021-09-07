f2b9428d10e3798908d6a3f62c1e80253e249aba
blitz2190 <blitz2190@DESKTOP-216274I>
2021-09-07 17:52:32 -0400

    broken fixed?

--- Src/main.c
+++ Src/main.c
@@ -497,7 +497,7 @@ void loadEEpromSettings(){
 	    }
 
 	   if(eepromBuffer[25] < 151 && eepromBuffer[25] > 49){
-		   minimum_duty_cycle = (eepromBuffer[25]/ 2) + (DEAD_TIME/3) + (eepromBuffer[26] / 20);
+		   minimum_duty_cycle = (eepromBuffer[25]/ 2) + (DEAD_TIME/3) + (eepromBuffer[26] / 50);
 		   min_startup_duty = minimum_duty_cycle;
 //		   if (use_sin_start){
 //			   min_startup_duty = eepromBuffer[25];
@@ -937,12 +937,12 @@ if (running){
 						     }else{
 						    	 minimum_duty_cycle--;
 						     }
-					 //if(minimum_duty_cycle > 200){
-					//	 minimum_duty_cycle = 200;
-					 //}
-					 //if(minimum_duty_cycle < (DEAD_TIME/2) + (eepromBuffer[25]/2)){        // boost minimum duty cycle by a small amount permanently too
-					//	 minimum_duty_cycle= (DEAD_TIME/2) + (eepromBuffer[25]/2);
-					 //}
+					 if(minimum_duty_cycle > 200){
+						 minimum_duty_cycle = 200;
+					 }
+					 if(minimum_duty_cycle < (DEAD_TIME/2) + (eepromBuffer[25]/2)){        // boost minimum duty cycle by a small amount permanently too
+						 minimum_duty_cycle= (DEAD_TIME/2) + (eepromBuffer[25]/2);
+					 }
 
 				velocity_count = 0;
 
@@ -1009,7 +1009,7 @@ if(desync_check && zero_crosses > 10){
 		desync_happened ++;
 //running = 0;
 //old_routine = 1;
-		last_duty_cycle = min_startup_duty;
+		last_duty_cycle = min_startup_duty/2;
 		}
 
 		desync_check = 0;
