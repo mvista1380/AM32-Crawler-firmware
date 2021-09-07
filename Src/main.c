7e971ee9624b47b3aae8374dc258378a6092c36d
blitz2190 <blitz2190@DESKTOP-216274I>
2021-09-07 12:48:00 -0400

    space

--- Src/main.c
+++ Src/main.c
@@ -853,7 +853,7 @@ if(!armed){
 		  prop_brake_active = 0;
 	  }
 	  }
-	  if (input < 47 + ((sine_mode_changeover * 0.75) * use_sin_start)){
+	  if (input < 47 + (((sine_mode_changeover / 4) * 3) * use_sin_start)){
 		if(play_tone_flag != 0){
 			if(play_tone_flag == 1){
 				playDefaultTone();
