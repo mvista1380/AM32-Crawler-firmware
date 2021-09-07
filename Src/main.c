d662b42e77e3ce1d68f01b4bd4b055224cbf49a3
blitz2190 <blitz2190@DESKTOP-216274I>
2021-09-07 15:57:12 -0400

    duty cycle again

--- Src/main.c
+++ Src/main.c
@@ -497,8 +497,8 @@ void loadEEpromSettings(){
 	    }
 
 	   if(eepromBuffer[25] < 151 && eepromBuffer[25] > 49){
-		   minimum_duty_cycle = (eepromBuffer[25]/ 2) + (DEAD_TIME/3) + (eepromBuffer[26] / 10);
-		   min_startup_duty = minimum_duty_cycle + startup_boost;
+		   minimum_duty_cycle = (eepromBuffer[25]/ 2) + (DEAD_TIME/3) + (eepromBuffer[26] / 5);
+		   min_startup_duty = minimum_duty_cycle;
 //		   if (use_sin_start){
 //			   min_startup_duty = eepromBuffer[25];
 //			   minimum_duty_cycle = eepromBuffer[25]/ 2;
