54343e20ed3be223b1ea9a9f87a34d7780f26bb9
blitz2190 <blitz2190@DESKTOP-216274I>
2021-09-08 19:15:55 -0400

    asdasd

--- Src/main.c
+++ Src/main.c
@@ -469,7 +469,11 @@ void loadEEpromSettings(){
 
 	if(eepromBuffer[25] < 151 && eepromBuffer[25] > 49){
 		minimum_duty_cycle = (eepromBuffer[25]/ 2) + (DEAD_TIME/3) + (eepromBuffer[26] / 2);
-		max_sin_inc = ((minimum_duty_cycle / (TIM1_AUTORELOAD + 1)) * 100) * 4; 
+		max_sin_inc = minimum_duty_cycle;
+		if (max_sin_inc > 120) {
+			max_sin_inc = 120;
+		}
+
 	}
 	else{
 		minimum_duty_cycle = 150;
