39550f5509a153192d61fcee4be45adf72ef17b2
blitz2190 <blitz2190@DESKTOP-216274I>
2021-09-07 20:22:22 -0400

    increase sine speed

--- Src/main.c
+++ Src/main.c
@@ -1598,7 +1598,7 @@ if(input >= 47 && armed){
 		maskPhaseInterrupts();
 		allpwm();
 		advanceincrement();
-		step_delay = map (input, 48, sine_mode_changeover, 7000/motor_poles, 700/motor_poles);
+		step_delay = map (input, 48, sine_mode_changeover, 7000/motor_poles, 20);
 
 		if (input >= sine_mode_changeover && phase_A_position == 0){
 			stepper_sine = 0;
