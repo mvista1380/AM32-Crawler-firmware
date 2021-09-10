372974cd0f69b3278cb6d48dd8c26d72ee7e04fc
blitz2190 <blitz2190@DESKTOP-216274I>
2021-09-10 15:46:46 -0400

    asdasd

--- Src/main.c
+++ Src/main.c
@@ -302,12 +302,14 @@ int bemf_timout_happened = 0;
 int timeout_count = 0;
 int bemf_timeout_threshold = 10;
 
-int changeover_step = 2;
+int changeover_step = 5;
 int filter_level = 5;
 int running = 0;
 int advance = 0;
 int advancedivisor = 6;
 char rising = 1;
+char amplitude = 150;
+char sin_cycle_complete = 0;
 
 ////Space Vector PWM ////////////////
 //const int pwmSin[] ={128, 132, 136, 140, 143, 147, 151, 155, 159, 162, 166, 170, 174, 178, 181, 185, 189, 192, 196, 200, 203, 207, 211, 214, 218, 221, 225, 228, 232, 235, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 248, 249, 250, 250, 251, 252, 252, 253, 253, 253, 254, 254, 254, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 254, 254, 254, 253, 253, 253, 252, 252, 251, 250, 250, 249, 248, 248, 247, 246, 245, 244, 243, 242, 241, 240, 239, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 248, 249, 250, 250, 251, 252, 252, 253, 253, 253, 254, 254, 254, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 254, 254, 254, 253, 253, 253, 252, 252, 251, 250, 250, 249, 248, 248, 247, 246, 245, 244, 243, 242, 241, 240, 239, 238, 235, 232, 228, 225, 221, 218, 214, 211, 207, 203, 200, 196, 192, 189, 185, 181, 178, 174, 170, 166, 162, 159, 155, 151, 147, 143, 140, 136, 132, 128, 124, 120, 116, 113, 109, 105, 101, 97, 94, 90, 86, 82, 78, 75, 71, 67, 64, 60, 56, 53, 49, 45, 42, 38, 35, 31, 28, 24, 21, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 8, 7, 6, 6, 5, 4, 4, 3, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 8, 7, 6, 6, 5, 4, 4, 3, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 24, 28, 31, 35, 38, 42, 45, 49, 53, 56, 60, 64, 67, 71, 75, 78, 82, 86, 90, 94, 97, 101, 105, 109, 113, 116, 120, 124};
@@ -1151,6 +1153,7 @@ void advanceincrement(int input){
 		phase_A_position += inc;
 		if (phase_A_position > 359){
 			phase_A_position -= 360;
+			sin_cycle_complete = 1;
 		}
 
 		phase_B_position += inc;
@@ -1167,6 +1170,7 @@ void advanceincrement(int input){
 		phase_A_position -= inc;
 		if (phase_A_position < 0){
 			phase_A_position += 360;
+			sin_cycle_complete = 1;
 		}
 
 		phase_B_position -= inc;
@@ -1180,11 +1184,9 @@ void advanceincrement(int input){
 		}
 	}
 
-	char current = map(input, 47, sine_mode_changeover, 80, 150);
-
-	TIM1->CCR1 = (current * pwmSin[0][phase_A_position]) + (current + 2);
-	TIM1->CCR2 = (current * pwmSin[1][phase_B_position]) + (current + 2);
-	TIM1->CCR3 = (current * pwmSin[2][phase_C_position]) + (current + 2);
+	TIM1->CCR1 = (amplitude * pwmSin[0][phase_A_position]) + (amplitude + 2);
+	TIM1->CCR2 = (amplitude * pwmSin[1][phase_B_position]) + (amplitude + 2);
+	TIM1->CCR3 = (amplitude * pwmSin[2][phase_C_position]) + (amplitude + 2);
 
 	//TIM1->CCR1 = ((2*pwmSin[phase_A_position]/SINE_DIVIDER)+ gate_drive_offset)*TIM1_AUTORELOAD/2000;
 	//TIM1->CCR2 = ((2*pwmSin[phase_B_position]/SINE_DIVIDER)+ gate_drive_offset)*TIM1_AUTORELOAD/2000;
@@ -1223,7 +1225,7 @@ void zcfoundroutine(){   // only used in polling mode, blocking routine.
 }
 
 void SwitchOver() {
-
+	sin_cycle_complete = 0;
 	stepper_sine = 0;
 	running = 1;
 	old_routine = 1;
@@ -1242,7 +1244,7 @@ void SwitchOver() {
 	TIM1->CCR2 = adjusted_duty_cycle;
 	TIM1->CCR3 = adjusted_duty_cycle;
 
-	step = 5;              // rising bemf on a same as position 0.	
+	step = changeover_step;              // rising bemf on a same as position 0.	
 	comStep(step);
 	changeCompInput();
 	enableCompInterrupts();
@@ -1582,7 +1584,7 @@ int main(void)
 				advanceincrement(input);
 				step_delay = map (input, 48, sine_mode_changeover, 350, 20);
 				
-				if (input > sine_mode_changeover && phase_A_position == 0){					
+				if (input > sine_mode_changeover && sin_cycle_complete == 1){
 					duty_cycle = map(input, sine_mode_changeover, 2047, minimum_duty_cycle, TIMER1_MAX_ARR);
 					SwitchOver();
 				}
