0b83be85e1f1551932420051fd05e66dacc290ea
blitz2190 <blitz2190@DESKTOP-216274I>
2021-09-10 11:01:59 -0400

    asdadsa

--- Src/main.c
+++ Src/main.c
@@ -1417,7 +1417,7 @@ int main(void)
 					last_average_interval = average_interval;
 					//  minimum_duty_cycle = ;
 					INTERVAL_TIMER->CNT = 9000;
-					zero_crosses = 99;
+					zero_crosses = 0;
 					prop_brake_active = 0;
 					step = 6;              // rising bemf on a same as position 0.
 					duty_cycle = map(input, sine_mode_changeover, 2047, minimum_duty_cycle, TIMER1_MAX_ARR);
