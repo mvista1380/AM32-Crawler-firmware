6c93e24cbfcb2c329acb46c5984d60f850b43338
blitz2190 <blitz2190@DESKTOP-216274I>
2021-09-10 19:00:14 -0400

    asdasd

--- Src/main.c
+++ Src/main.c
@@ -302,7 +302,7 @@ int bemf_timout_happened = 0;
 int timeout_count = 0;
 int bemf_timeout_threshold = 10;
 
-int changeover_step = 6;
+int changeover_step = 1;
 int filter_level = 5;
 int running = 0;
 int advance = 0;
