99e40a671d932a7bbaf984a146a81ebe783d149e
blitz2190 <blitz2190@DESKTOP-216274I>
2021-09-12 15:30:17 -0400

    asdasd

--- Src/main.c
+++ Src/main.c
@@ -308,7 +308,7 @@ int running = 0;
 int advance = 0;
 int advancedivisor = 6;
 char rising = 1;
-char amplitude = 150;
+char amplitude = 160;
 char sin_cycle_complete = 0;
 
 ////Space Vector PWM ////////////////
