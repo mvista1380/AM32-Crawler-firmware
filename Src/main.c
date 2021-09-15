aff37ebe94c5bd3f205803b95675a71a247c55f4
blitz2190 <blitz2190@DESKTOP-216274I>
2021-09-15 09:27:44 -0400

    asdasd

--- Src/main.c
+++ Src/main.c
@@ -315,6 +315,7 @@ char last_inc = 1;
 char stepper_sine = 0;
 char max_sin_inc = 3;
 char sin_stall_count = 0;
+char switchover_count = 0;
 
 ////Space Vector PWM ////////////////
 //const int pwmSin[] ={128, 132, 136, 140, 143, 147, 151, 155, 159, 162, 166, 170, 174, 178, 181, 185, 189, 192, 196, 200, 203, 207, 211, 214, 218, 221, 225, 228, 232, 235, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 248, 249, 250, 250, 251, 252, 252, 253, 253, 253, 254, 254, 254, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 254, 254, 254, 253, 253, 253, 252, 252, 251, 250, 250, 249, 248, 248, 247, 246, 245, 244, 243, 242, 241, 240, 239, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 248, 249, 250, 250, 251, 252, 252, 253, 253, 253, 254, 254, 254, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 254, 254, 254, 253, 253, 253, 252, 252, 251, 250, 250, 249, 248, 248, 247, 246, 245, 244, 243, 242, 241, 240, 239, 238, 235, 232, 228, 225, 221, 218, 214, 211, 207, 203, 200, 196, 192, 189, 185, 181, 178, 174, 170, 166, 162, 159, 155, 151, 147, 143, 140, 136, 132, 128, 124, 120, 116, 113, 109, 105, 101, 97, 94, 90, 86, 82, 78, 75, 71, 67, 64, 60, 56, 53, 49, 45, 42, 38, 35, 31, 28, 24, 21, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 8, 7, 6, 6, 5, 4, 4, 3, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 8, 7, 6, 6, 5, 4, 4, 3, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 24, 28, 31, 35, 38, 42, 45, 49, 53, 56, 60, 64, 67, 71, 75, 78, 82, 86, 90, 94, 97, 101, 105, 109, 113, 116, 120, 124};
@@ -941,8 +942,14 @@ void tenKhzRoutine(){
 				GPIOB->BSRR = LL_GPIO_PIN_5;  // on blue
 				#endif
 			}
-	  
-			duty_cycle = map(input, sine_mode_changeover, 2047, minimum_duty_cycle, TIMER1_MAX_ARR);
+			
+			if (switchover_count < 10) {
+				duty_cycle = map(input, sine_mode_changeover, 2047, (minimum_duty_cycle / 10) * 12, TIMER1_MAX_ARR);
+				switchover_count++;
+			}
+			else
+				duty_cycle = map(input, sine_mode_changeover, 2047, minimum_duty_cycle, TIMER1_MAX_ARR);
+
 			prop_brake_active = 0;
 		}
 
@@ -1246,6 +1253,7 @@ void SwitchOver() {
 	running = 1;
 	old_routine = 0;
 	prop_brake_active = 0;
+	switchover_count = 0;
 	//commutation_interval = 9000;
 	//average_interval = 9000;
 	last_average_interval = average_interval;
@@ -1602,7 +1610,7 @@ int main(void)
 				step_delay = map (input, 48, sine_mode_changeover, 300, 20);
 				
 				if (input > sine_mode_changeover && sin_cycle_complete == 1){
-					duty_cycle = map(input, sine_mode_changeover, 2047, (minimum_duty_cycle / 10)  * 11, TIMER1_MAX_ARR);
+					duty_cycle = map(input, sine_mode_changeover, 2047, (minimum_duty_cycle / 10)  * 12, TIMER1_MAX_ARR);
 					SwitchOver();
 				}
 				else {
