a3ae05ed9276a197c948e30eb9776f663852208b
blitz2190 <blitz2190@DESKTOP-216274I>
2021-09-16 09:35:20 -0400

    asdasd

--- Src/main.c
+++ Src/main.c
@@ -947,8 +947,8 @@ void tenKhzRoutine(){
 				#endif
 			}
 			
-			if (switchover_count < 20) {
-				duty_cycle = map(input, sine_mode_changeover, 2047, (minimum_duty_cycle / 10) * 13, TIMER1_MAX_ARR);
+			if (switchover_count < 40) {
+				duty_cycle = map(input, sine_mode_changeover, 2047, minimum_duty_cycle * 2, TIMER1_MAX_ARR);
 				switchover_count++;
 			}
 			else
@@ -1629,7 +1629,7 @@ int main(void)
 				step_delay = map (input, 48, sine_mode_changeover, 300, 20);
 				
 				if (input > sine_mode_changeover && sin_cycle_complete == 1){
-					duty_cycle = map(input, sine_mode_changeover, 2047, (minimum_duty_orig / 10)  * 13, TIMER1_MAX_ARR);
+					duty_cycle = map(input, sine_mode_changeover, 2047, minimum_duty_orig * 2, TIMER1_MAX_ARR);
 					SwitchOver();
 				}
 				else {
