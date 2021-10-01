85242ab49a50fe41a810f0070a4ef9c87512b142
blitz2190 <blitz2190@DESKTOP-216274I>
2021-10-01 13:39:47 -0400

    IFlight 80A voltage divider

--- Inc/targets.h
+++ Inc/targets.h
@@ -44,7 +44,7 @@
 #define FIRMWARE_NAME           "IFlight_80A "
 #define DEAD_TIME               45
 #define HARDWARE_GROUP_F0_B
-#define TARGET_VOLTAGE_DIVIDER  200
+#define TARGET_VOLTAGE_DIVIDER  225
 #endif
 
 #ifdef MP6531
