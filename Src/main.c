c0b6605b648bc934988786501b586961534a7496
blitz2190 <blitz2190@DESKTOP-216274I>
2021-10-06 19:47:04 -0400

    Throttle Calibration

--- Src/main.c
+++ Src/main.c
@@ -1344,7 +1344,7 @@ int main(void)
 
 				last_input = newinput;
 
-				if (timout_counter >= 10000)
+				if (timout_counter >= 4000)
 					throttle_learn_active = 0;
 
 				if (newinput > current_max)
