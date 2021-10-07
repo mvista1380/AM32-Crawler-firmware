3c094f6d09af9f294e6ac9d9bc546f9d67e08f9d
blitz2190 <blitz2190@DESKTOP-216274I>
2021-10-06 20:41:28 -0400

    asdasd

--- Src/main.c
+++ Src/main.c
@@ -4,16 +4,114 @@
 //=============================== Changelog =================================
 //===========================================================================
 /*
-* forked from AM32 at version 1.75
+* 1.54 Changelog;
+* --Added firmware name to targets and firmware version to main
+* --added two more dshot to beacons 1-3 currently working
+* --added KV option to firmware, low rpm power protection is based on KV
+* --start power now controls minimum idle power as well as startup strength.
+* --change default timing to 22.5
+* --Lowered default minimum idle setting to 1.5 percent duty cycle, slider range from 1-2.
+* --Added dshot commands to save settings and reset ESC.
 *
-*Crawler Only - AM32 Based
-* V1.0	- Mass cleanup of all none crawler related code
-		- Sine Mode variable speed/current
-		- Thermal shutdown as well as thermal throttling pre shutdown
-		- Throttle Auto Calibration
-		- completely new stall compensation code
-		- simplified config variables
-		- sine trap switchover general smoothness
+*1.56 Changelog.
+* -- added check to stall protection to wait until after 40 zero crosses to fix high startup throttle hiccup.
+* -- added TIMER 1 update interrupt and PWM changes are done once per pwm period
+* -- reduce commutation interval averaging length
+* -- reduce false positive filter level to 2 and eliminate threshold where filter is stopped.
+* -- disable interrupt before sounds
+* -- disable TIM1 interrupt during stepper sinusoidal mode
+* -- add 28us delay for dshot300
+* -- report 0 rpm until the first 10 successful steps.
+* -- move serial ADC telemetry calculations and desync check to 10Khz interrupt.
+*
+* 1.57
+* -- remove spurious commutations and rpm data at startup by polling for longer interval on startup
+*
+* 1.58
+* -- move signal timeout to 10khz routine and set armed timeout to one quarter second 2500 / 10000
+* 1.59
+* -- moved comp order definitions to target.h
+* -- fixed update version number if older than new version
+* -- cleanup, moved all input and output to IO.c
+* -- moved comparator functions to comparator.c
+* -- removed ALOT of useless variables
+* -- added siskin target
+* -- moved pwm changes to 10khz routine
+* -- moved basic functions to functions.c
+* -- moved peripherals setup to periherals.c
+* -- added crawler mode settings
+*
+* 1.60
+* -- added sine mode hysteresis
+* -- increased power in stall protection and lowered start rpm for crawlers
+* -- removed onehot125 from crawler mode
+* -- reduced maximum startup power from 400 to 350
+* -- change minimum duty cycle to DEAD_TIME
+* -- version and name moved to permanent spot in FLASH memory, thanks mikeller
+*
+* 1.61
+* -- moved duty cycle calculation to 10khz and added max change option.
+* -- decreased maximum interval change to 25%
+* -- reduce wait time on fast acceleration (fast_accel)
+* -- added check in interrupt for early zero cross
+*
+* 1.62
+* --moved control to 10khz loop
+* --changed condition for low rpm filter for duty cycle from || to &&
+* --introduced max deceleration and set it to 20ms to go from 100 to 0
+* --added configurable servo throttle ranges
+*
+*
+*1.63
+*-- increase time for zero cross error detection below 250us commutation interval
+*-- increase max change a low rpm x10
+*-- set low limit of throttle ramp to a lower point and increase upper range
+*-- change desync event from full restart to just lower throttle.
+
+*1.64
+* --added startup check for continuous high signal, reboot to enter bootloader.
+*-- added brake on stop from eeprom
+*-- added stall protection from eeprom
+*-- added motor pole divider for sinusoidal and low rpm power protection
+*-- fixed dshot commands, added confirmation beeps and removed blocking behavior
+*--
+*1.65
+*-- Added 32 millisecond telemetry output
+*-- added low voltage cutoff , divider value and cutoff voltage needs to be added to eeprom
+*-- added beep to indicate cell count if low voltage active
+*-- added current reading on pa3 , conversion factor needs to be added to eeprom
+*-- fixed servo input capture to only read positive pulse to handle higher refresh rates.
+*-- disabled oneshot 125.
+*-- extended servo range to match full output range of receivers
+*-- added RC CAR style reverse, proportional brake on first reverse , double tap to change direction
+*-- added brushed motor control mode
+*-- added settings to EEPROM version 1
+*-- add gimbal control option.
+*--
+*1.66
+*-- move idwg init to after input tune
+*-- remove reset after save command -- dshot
+*-- added wraith32 target
+*-- added average pulse check for signal detection
+*--
+*1.67
+*-- Rework file structure for multiple MCU support
+*-- Add g071 mcu
+*--
+*1.68
+*--increased allowed average pulse length to avoid double startup
+*1.69
+*--removed line re-enabling comparator after disabling.
+*1.70 fix dshot for Kiss FC
+*1.71 fix dshot for Ardupilot / Px4 FC
+*1.72 Fix telemetry output and add 1 second arming.
+*1.73 Fix false arming if no signal. Remove low rpm throttle protection below 300kv
+*1.74 Add Sine Mode range and drake brake strength adjustment
+*1.75 Disable brake on stop for PWM_ENABLE_BRIDGE 
+Removed automatic brake on stop on neutral for RC car proportional brake.
+Adjust sine speed and stall protection speed to more closely match
+makefile fixes from Cruwaller 
+Removed gd32 build, until firmware is functional
 */
 #include <stdint.h>
 #include "main.h"
@@ -34,7 +132,7 @@
 //===========================================================================
 
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 0
+#define VERSION_MINOR 75
 char dir_reversed = 0;
 char brake_on_stop = 1;
 char program_running = 1; //low voltage turns off main loop
@@ -1032,60 +1130,6 @@ void SwitchOver() {
 	enableCompInterrupts();
 }
 
-void RunThrottleCalibrate() {
-	allOff();
-	playLearnModeTune();
-	int starting_input = newinput;
-	int current_max = newinput;
-	int current_min = newinput;
-	int last_input = newinput;
-	int timout_counter = 0;
-	char changed = 0;
-	throttle_learn_active = 1;
-
-	while (throttle_learn_active) {
-		LL_IWDG_ReloadCounter(IWDG);
-		signaltimeout = 0;
-
-#ifdef USE_ADC_INPUT
-		ADC_smoothed_input = (((10 * ADC_smoothed_input) + ADC_raw_input) / 11);
-		newinput = ADC_smoothed_input / 2;
-		if (newinput > 2000) {
-			newinput = 2000;
-		}
-#endif
-
-		if (getAbsDif(last_input, newinput) < 10)
-			timout_counter++;
-		else {
-			changed = 1;
-			timout_counter = 0;
-		}
-
-		last_input = newinput;
-
-		if (timout_counter >= 4000)
-			throttle_learn_active = 0;
-
-		if (newinput > current_max)
-			current_max = newinput;
-
-		if (newinput < current_min)
-			current_min = newinput;
-
-		delayMillis(1);
-	}
-
-	if (changed) {
-		eepromBuffer[32] = (current_min - 750) / 2;
-		eepromBuffer[33] = (current_max - 1750) / 2;
-		eepromBuffer[34] = ((current_min + current_max) / 2) - 1374;
-		saveEEpromSettings();
-	}
-
-	playEndLearnModeTune();
-}
-
 int main(void)
 {
 	initAfterJump();
@@ -1195,11 +1239,6 @@ int main(void)
 		temperature_offset = 230;
 	}
 	#endif
-
-	if (!armed && newinput > (1000 + (servo_dead_band << 1))) {
-		RunThrottleCalibrate();
-	}
-
 	while (program_running){
 
 		LL_IWDG_ReloadCounter(IWDG);
@@ -1272,6 +1311,60 @@ int main(void)
 		}
 		#endif
 		stuckcounter = 0;
+
+		if (!armed && newinput > (1000 + (servo_dead_band << 1))) {
+			allOff();
+			playLearnModeTune();
+			int starting_input = newinput;
+			int current_max = newinput;
+			int current_min = newinput;
+			int last_input = newinput;
+			int timout_counter = 0;
+			char changed = 0;
+			throttle_learn_active = 1;
+			
+			while (throttle_learn_active) {
+				LL_IWDG_ReloadCounter(IWDG);
+				signaltimeout = 0;
+
+				#ifdef USE_ADC_INPUT
+				ADC_smoothed_input = (((10 * ADC_smoothed_input) + ADC_raw_input) / 11);
+				newinput = ADC_smoothed_input / 2;
+				if (newinput > 2000) {
+					newinput = 2000;
+				}
+				#endif
+
+				if (getAbsDif(last_input, newinput) < 10)
+					timout_counter++;
+				else {
+					changed = 1;
+					timout_counter = 0;
+				}
+
+				last_input = newinput;
+
+				if (timout_counter >= 10000)
+					throttle_learn_active = 0;
+
+				if (newinput > current_max)
+					current_max = newinput;
+
+				if (newinput < current_min)
+					current_min = newinput;
+
+				delayMillis(1);
+			}
+			
+			if (changed) {
+				eepromBuffer[32] = (current_min - 750) / 2;
+				eepromBuffer[33] = (current_max - 1750) / 2;
+				eepromBuffer[34] = ((current_min + current_max) / 2) - 1374;
+				saveEEpromSettings();
+			}
+
+			playEndLearnModeTune();
+		}
 
 		if (dshot == 0){
 			if (newinput > (1000 + (servo_dead_band<<1))) {
