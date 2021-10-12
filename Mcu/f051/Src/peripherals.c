1ac061aa1ab0eb31ac5ebc8ddf2dd06ee289c9a5
blitz2190 <blitz2190@DESKTOP-216274I>
2021-10-12 18:48:09 -0400

    aasdasd

--- Mcu/f051/Src/peripherals.c
+++ Mcu/f051/Src/peripherals.c
@@ -160,7 +160,7 @@ void MX_IWDG_Init(void)
   LL_IWDG_Enable(IWDG);
   LL_IWDG_EnableWriteAccess(IWDG);
   LL_IWDG_SetPrescaler(IWDG, LL_IWDG_PRESCALER_16);
-  LL_IWDG_SetReloadCounter(IWDG, 4000);
+  LL_IWDG_SetReloadCounter(IWDG, 4999);
   while (LL_IWDG_IsReady(IWDG) != 1)
   {
   }
