ca677987a935c84a0b9d3ff782a7cf0a832a1910
blitz2190 <blitz2190@DESKTOP-216274I>
2021-10-14 17:43:08 -0400

    pid stuff

--- Src/main.c
+++ Src/main.c
@@ -795,10 +795,10 @@ void tenKhzRoutine(){
 					p_error = commutation_interval - (minimum_commutation - 100); // buffer so it doesnt bounce
 					p_error_integral = p_error_integral + p_error;
 
-					if (p_error_integral > 25)
-						p_error_integral = 25;
-					else if (p_error_integral < -25)
-						p_error_integral = -25;
+					if (p_error_integral > 100)
+						p_error_integral = 100;
+					else if (p_error_integral < -100)
+						p_error_integral = -100;
 
 					p_error_derivative = p_error - p_prev_rror;
 					p_prev_rror = p_error;
