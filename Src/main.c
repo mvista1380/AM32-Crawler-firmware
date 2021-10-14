d9929090bfe719e974eeeca7a70cb6db17f9e0bd
blitz2190 <blitz2190@DESKTOP-216274I>
2021-10-14 17:17:02 -0400

    pid

--- Src/main.c
+++ Src/main.c
@@ -784,41 +784,35 @@ void tenKhzRoutine(){
 
 			if (running){
 
-				if (commutation_interval > minimum_commutation)
-					enable_pid = 1;
+				//if (commutation_interval > minimum_commutation)
+					//enable_pid = 1;
 				
-				if (enable_pid) {
-					pid_update_count++;
-					if (pid_update_count == 50) {
-						pid_update_count = 0;
+				//if (enable_pid) {
+				pid_update_count++;
+				if (pid_update_count == 100) {
+					pid_update_count = 0;
 
-						p_error = commutation_interval - (minimum_commutation - 100); // buffer so it doesnt bounce
-						p_error_integral = p_error_integral + p_error;
+					p_error = commutation_interval - (minimum_commutation - 100); // buffer so it doesnt bounce
+					p_error_integral = p_error_integral + p_error;
 
-						if (p_error_integral > 1000)
-							p_error_integral = 1000;
-						else if (p_error_integral < -1000)
-							p_error_integral = -1000;
+					if (p_error_integral > 1000)
+						p_error_integral = 1000;
+					else if (p_error_integral < -1000)
+						p_error_integral = -1000;
 
-						p_error_derivative = p_error - p_prev_rror;
-						p_prev_rror = p_error;
+					p_error_derivative = p_error - p_prev_rror;
+					p_prev_rror = p_error;
 
-						minimum_duty_cycle = (K_p_duty * p_error) + (K_i_duty * p_error_integral) + (K_d_duty * p_error_derivative);
+					minimum_duty_cycle = (K_p_duty * p_error) + (K_i_duty * p_error_integral) + (K_d_duty * p_error_derivative);
 
-						if (minimum_duty_cycle > maximum_duty_orig)
-							minimum_duty_cycle = maximum_duty_orig;
-						else if (minimum_duty_cycle < starting_duty_orig) {
-							minimum_duty_cycle = starting_duty_orig;
-							enable_pid = 0;
-						}
+					if (minimum_duty_cycle > maximum_duty_orig)
+						minimum_duty_cycle = maximum_duty_orig;
+					else if (minimum_duty_cycle < starting_duty_orig) {
+						minimum_duty_cycle = starting_duty_orig;
+						enable_pid = 0;
 					}
 				}
-				else if (p_error != 0) {
-					p_error = 0;
-					p_error_integral = 0;
-					p_error_derivative = 0;
-					p_prev_rror = 0;
-				}
+				//}
 				/*
 				if (stall_detected) {
 					if (duty_cycle_ramp_down_count < duty_cycle_ramp_down_delay)
