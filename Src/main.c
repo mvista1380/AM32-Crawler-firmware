5b31a39b28e3ff472dcd0c5e8fe89b2c24c3badc
blitz2190 <blitz2190@DESKTOP-216274I>
2021-10-15 13:11:53 -0400

    pid

--- Src/main.c
+++ Src/main.c
@@ -160,7 +160,6 @@ uint16_t minimum_duty_cycle = DEAD_TIME;
 uint16_t maximum_duty_cycle = DEAD_TIME;
 uint16_t starting_duty_orig = DEAD_TIME;
 uint16_t maximum_duty_orig = DEAD_TIME;
-uint16_t p_duty_cycle_max = DEAD_TIME;
 uint16_t duty_cycle_multiplier = 210; //130 = 30% power increase
 char desync_check = 0;
 char low_kv_filter_level = 20;
@@ -477,7 +476,7 @@ void loadEEpromSettings(){
 
 	starting_duty_orig = minimum_duty_cycle;
 	maximum_duty_orig = (starting_duty_orig / 100) * duty_cycle_multiplier;
-	p_duty_cycle_max = maximum_duty_cycle + (maximum_duty_cycle - (maximum_duty_cycle * K_p_duty));
+	//p_duty_cycle_max = maximum_duty_cycle + (maximum_duty_cycle - (maximum_duty_cycle * K_p_duty));
 	maximum_duty_cycle = (TIMER1_MAX_ARR / 100) * 85;
 
 
@@ -801,7 +800,7 @@ void tenKhzRoutine(){
 					pid_update_count = 0;
 					raw_error = commutation_interval - minimum_commutation;
 
-					p_error = map(raw_error, -2000, 2000, -p_duty_cycle_max, p_duty_cycle_max);
+					p_error = map(raw_error, -2000, 2000, -maximum_duty_orig, maximum_duty_orig);
 
 					p_error_integral += (p_error * 10); //10 millisecond interval
 
