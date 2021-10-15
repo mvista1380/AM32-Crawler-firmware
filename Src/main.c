5f59a469d314d12501f66767cb3aa0334008adbf
blitz2190 <blitz2190@DESKTOP-216274I>
2021-10-15 11:48:17 -0400

    version

--- Src/main.c
+++ Src/main.c
@@ -42,7 +42,7 @@
 //===========================================================================
 
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 10
+#define VERSION_MINOR 01
 char dir_reversed = 0;
 char brake_on_stop = 1;
 char program_running = 1; //low voltage turns off main loop
