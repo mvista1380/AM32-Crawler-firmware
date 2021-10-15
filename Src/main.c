6a6c6cadcb4cf4a2a35a66cfbf1cc00ead9a6731
blitz2190 <blitz2190@DESKTOP-216274I>
2021-10-15 14:24:13 -0400

    asd

--- Src/main.c
+++ Src/main.c
@@ -98,7 +98,7 @@ char maximum_throttle_change_ramp = 1;
 
 float K_p_duty = 0.3f;
 float K_i_duty = 0.1f;
-float K_d_duty = 0f;
+float K_d_duty = 0.0f;
 
 float p_error_integral = 0;
 float p_error_derivative = 0;
