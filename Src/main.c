49fd7728ff84f4a00c438339d75bb2e712554987
blitz2190 <blitz2190@DESKTOP-216274I>
2021-10-15 11:15:11 -0400

    pid tunning

--- Src/main.c
+++ Src/main.c
@@ -91,8 +91,8 @@ uint16_t armed_timeout_count;
 uint8_t desync_happened = 0;
 char maximum_throttle_change_ramp = 1;
 
-float K_p_duty = 0.83;
-float K_i_duty = 0.15;
+float K_p_duty = 0.82;
+float K_i_duty = 0.13;
 float K_d_duty = 0.1;
 
 float p_error_integral = 0;
