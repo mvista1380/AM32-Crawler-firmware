da40337487eee261a04608e97ebd856739e4833a
blitz2190 <blitz2190@DESKTOP-216274I>
2021-10-15 11:27:34 -0400

    asdasd

--- Src/main.c
+++ Src/main.c
@@ -91,7 +91,7 @@ uint16_t armed_timeout_count;
 uint8_t desync_happened = 0;
 char maximum_throttle_change_ramp = 1;
 
-float K_p_duty = 0.82;
+float K_p_duty = 0.8;
 float K_i_duty = 0.13;
 float K_d_duty = 0.1;
 
