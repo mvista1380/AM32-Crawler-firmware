2538c6113c26aecf3cfe2da723d06138b3e6bd66
blitz2190 <blitz2190@DESKTOP-216274I>
2021-10-18 14:29:45 -0400

    testing

--- Src/main.c
+++ Src/main.c
@@ -1355,7 +1355,7 @@ int main(void)
 			adc_counter = 0;
 			
 			if (degrees_celsius >= 115 && armed) {
-				if (thermal_protection_active == 0) {
+				/*if (thermal_protection_active == 0) {
 					allOff();
 					maskPhaseInterrupts();
 					eepromBuffer[44] = converted_degrees >> 8;
@@ -1374,8 +1374,8 @@ int main(void)
 				playThermalWarningTune();
 				last_error = 2;
 
-				if(thermal_protection_active ==0)
-					saveEEpromSettings();				
+				//if(thermal_protection_active ==0)
+					//saveEEpromSettings();				
 				
 				thermal_protection_active = 1;
 				short thermal_counter = 1500;
