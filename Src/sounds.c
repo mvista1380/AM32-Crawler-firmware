d36023375cfe0be60b0e3cf6d48843f511ff5a7e
blitz2190 <blitz2190@DESKTOP-216274I>
2021-10-19 10:10:07 -0400

    asdasd

--- Src/sounds.c
+++ Src/sounds.c
@@ -168,14 +168,8 @@ void playThermalWarningTune() {
 	delayMillis(200);         // duration of beep
 	comStep(5);
 
-	LL_IWDG_ReloadCounter(IWDG);
-	signaltimeout = 0;
-
 	TIM1->PSC = 25;            // next beep is higher frequency
 	delayMillis(200);
-
-	LL_IWDG_ReloadCounter(IWDG);
-	signaltimeout = 0;
 
 	comStep(6);
 	TIM1->PSC = 25;         // higher again..
