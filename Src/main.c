5024425aa8dd341f2e047665a2bbf948a55d1109
blitz2190 <blitz2190@DESKTOP-216274I>
2021-10-18 22:54:59 -0400

    asdasd

--- Src/main.c
+++ Src/main.c
@@ -173,7 +173,7 @@ typedef struct __attribute__((packed)) {
 firmware_info_s __attribute__ ((section(".firmware_info"))) firmware_info = {
   version_major: VERSION_MAJOR,
   version_minor: VERSION_MINOR,
-  device_name: FIRMWARE_NAME
+  device_name: "Testing Save"
 };
 
 uint8_t EEPROM_VERSION;
