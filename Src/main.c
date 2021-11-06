30f0efd10b0dbb025a98efa2f8ef788e393aaa21
blitz2190 <blitz2190@DESKTOP-216274I>
2021-11-06 13:59:53 -0400

    asdadsx

--- Src/main.c
+++ Src/main.c
@@ -747,7 +747,7 @@ void tenKhzRoutine(){
 				boost = (int)((K_p_duty * p_error) + (K_i_duty * p_error_integral) + (K_d_duty * p_error_derivative));
 
 				stuckcounter++; //full stall, adds a biiger boost
-				if (stuckcounter > 3000) {
+				if (stuckcounter > 4000) {
 					stall_boost += 1;
 				}
 
