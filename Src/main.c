682536a0f8cd14b327d706806c5294b9fba3919a
blitz2190 <blitz2190@DESKTOP-216274I>
2021-11-06 13:32:15 -0400

    asdasd

--- Src/main.c
+++ Src/main.c
@@ -747,8 +747,8 @@ void tenKhzRoutine(){
 				boost = (int)((K_p_duty * p_error) + (K_i_duty * p_error_integral) + (K_d_duty * p_error_derivative));
 
 				stuckcounter++; //full stall, adds a biiger boost
-				if (stuckcounter > 1500) {
-					stall_boost += 5;
+				if (stuckcounter > 2500) {
+					stall_boost += 2;
 				}
 
 				minimum_duty_cycle = starting_duty_orig + boost + stall_boost;
