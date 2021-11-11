f7507e6106d753c428790baa61069aa8dbf19cdc
blitz2190 <blitz2190@DESKTOP-216274I>
2021-11-11 07:41:39 -0500

    rdwsasda

--- Src/main.c
+++ Src/main.c
@@ -211,8 +211,8 @@ char dshot = 0;
 char servoPwm = 0;
 char step = 1;
 
-float K_p_duty = 0.08;
-float K_i_duty = 0.001;
+float K_p_duty = 0.1;
+float K_i_duty = 0.01;
 float K_d_duty = 0;
 float p_error_integral = 0;
 float p_error_derivative = 0;
